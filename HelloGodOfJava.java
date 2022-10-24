f03ce8e7813a9bd7055629e0634f244e780b17c4
shinjeha <shinjeha1993@gmail.com>
2022-10-24 23:59:30 +0900

    first commit

---
+++ HelloGodOfJava.java
@@ -0,0 +1,6 @@
+public class HelloGodOfJava {
+    public static void main(String[] args) { // 매개변수 String[] args. 매개변수 이름 변경 가능 args -> jeha
+        System.out.println("IHello God Of Java"); // out 필드는 static이니까 불러올 수 있는거지
+        System.out.println("I love this book");
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# 자바의 신
+읽고 따라하기 노트
+
+## compile 한글 에러 인코딩
+javac Hello.java -encoding UTF-8
